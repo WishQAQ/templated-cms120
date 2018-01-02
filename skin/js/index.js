67374425fa62b03432363f14714f03197bb9f818
Wish_Time <wish-time@outlook.com>
2018-01-02 13:42:23 +0800

    Initial commit

---
+++ skin/js/index.js
@@ -0,0 +1,26 @@
+;(function () {
+	'use strict';
+	var sliderMain = function() {
+		
+	  	$('#fh5co-hero .flexslider').flexslider({
+			animation: "fade",
+			slideshowSpeed: 5000,
+			directionNav: true,
+			start: function(){ },
+			before: function(){ }
+
+	  	});
+
+	};
+
+	$(function(){
+
+		sliderMain();
+		
+		//内容加载后的运动效果
+	    dataAnimate();	
+
+	});
+
+
+}());
